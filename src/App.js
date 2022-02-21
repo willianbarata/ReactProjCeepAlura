26b23962984439cc04e20608e3237f31bad471b6
willianbarata <willianbarata@gmail.com>
2022-02-21 20:07:17 -0300

    Estilizando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import  ListaDeNotas  from "./components/ListaDeNotas";
-
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
-  render(){
+  render() {
     return (
-      <section>
-     <FormularioCadastro/>
-     <ListaDeNotas />
-    </section>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
     );
   }
 }
