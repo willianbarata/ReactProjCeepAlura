17a402ff4865e55c2d39b6d033b7d3f816a3cbf2
willianbarata <willianbarata@gmail.com>
2022-02-21 20:24:00 -0300

    Aula 7 - lidando com eventos

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+
+  constructor(){
+    super();
+    this.titulo = "";
+  }
+
+  handlerMudancaTitulo(evento){
+    this.titulo = evento.target.value;
+  }
+
   render() {
     return (
       <form className="form-cadastro ">
@@ -8,6 +18,7 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handlerMudancaTitulo}
         />
         <textarea
           rows={15}
