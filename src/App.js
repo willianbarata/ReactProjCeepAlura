1d834c7854e8818a4b6b34bfce1f0ca62b09c9f1
willianbarata <willianbarata@gmail.com>
2022-02-21 21:21:57 -0300

    Aula 11 - entendendo state

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ class App extends Component {
   constructor(){
     super();
     this.notas = [];
+    this.state = {notas: []};
   }
 
   criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
-    console.log(this.notas.length);
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.notas}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
